afb6d13ef2ee379cc40cdad66458d30226c71d60
muhammad fajar <invasionfajar@gmail.com>
2023-04-26 21:10:33 +0700

    Materi: Array, Collection, Lopping, Hash

--- src/main/java/org/kojaydev/materi/collection/MapArray.java
+++ src/main/java/org/kojaydev/materi/collection/MapArray.java
@@ -9,6 +9,7 @@ public class MapArray {
 
         planets.put("key 1", new Planet("Earth", 0.03));
         planets.put("Key 2", new Planet("Mars", 0.44));
+        planets.put("Key 3", new Planet("Merkurius", 0.44));
 
         for(Planet planet : planets.values()){
             System.out.println(planet);
